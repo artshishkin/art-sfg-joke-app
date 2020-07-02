62f7699e8ac1c503861cb6a91cbe56b3f2a768d6
admin <d.art.shishkin@gmail.com>
2020-07-02 17:20:52 +0300

    art-sfg-joke-app-5 Map context root to Jokes view #5

--- src/main/java/com/artarkatesoft/artsfgjokeapp/controllers/JokesController.java
+++ src/main/java/com/artarkatesoft/artsfgjokeapp/controllers/JokesController.java
@@ -1,9 +1,15 @@
 package com.artarkatesoft.artsfgjokeapp.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class JokesController {
+
+    @GetMapping({"/", ""})
+    public String getJokesView() {
+        return null;
+    }
 
 
 }
