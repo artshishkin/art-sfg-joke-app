2f2f71b3f0788d9010bfee5926c010437df133f0
admin <d.art.shishkin@gmail.com>
2020-07-02 17:26:53 +0300

    art-sfg-joke-app-6  Set property 'joke' (closes #6)

--- src/main/java/com/artarkatesoft/artsfgjokeapp/controllers/JokesController.java
+++ src/main/java/com/artarkatesoft/artsfgjokeapp/controllers/JokesController.java
@@ -1,14 +1,22 @@
 package com.artarkatesoft.artsfgjokeapp.controllers;
 
+import com.artarkatesoft.artsfgjokeapp.services.JokeService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class JokesController {
 
-    @GetMapping({"/", ""})
-    public String getJokesView() {
+    private final JokeService jokeService;
 
+    public JokesController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @GetMapping({"/", ""})
+    public String getJokesView(Model model) {
+        model.addAttribute("joke",jokeService.getJoke());
         return null;
     }
 
