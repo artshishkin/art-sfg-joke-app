0807806fc1e0e9bee063df456d4ce53f4e858016
admin <d.art.shishkin@gmail.com>
2020-07-02 16:37:28 +0300

    added spring boot project closes #1

---
+++ src/main/java/com/artarkatesoft/artsfgjokeapp/ArtSfgJokeAppApplication.java
@@ -0,0 +1,13 @@
+package com.artarkatesoft.artsfgjokeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtSfgJokeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtSfgJokeAppApplication.class, args);
+	}
+
+}
